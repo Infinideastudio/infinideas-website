744d6f712ae3de299b5a9088e75524379d640ab9
ShangHuo <shanghuo@studio.snang.cc>
2025-04-24 13:46:01 +0800

    Internal属性不起作用，尝试让它起作用 (#28)
    Internal属性不起作用，尝试让它起作用

--- src/apps/document/components/Link.js
+++ src/apps/document/components/Link.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Link = (props) => {
     return (
-        props.Internal
+        props.hyper.Internal
             ? (<a href={props.hyper.Address}>{props.hyper.Text}</a>)
             : (<a href={props.hyper.Address} target="_blank" rel="noopener noreferrer">{props.hyper.Text}</a>)
     );
